90e4463074218967c0111b47f2aa3219213b2bec
yuriy.gorshkov <yuriy.gorshkov@gmail.com>
2014-02-03 22:50:22 +0400

    Xcode project

---
+++ ProjectEuler/Problem1/Problem1.cpp
@@ -0,0 +1,9 @@
+//
+//  Problem1.cpp
+//  ProjectEuler
+//
+//  Created by yury on 03/02/14.
+//  Copyright (c) 2014 yury. All rights reserved.
+//
+
+#include "Problem1.h"
---
+++ ProjectEuler/Problem1/Problem1.h
@@ -0,0 +1,14 @@
+//
+//  Problem1.h
+//  ProjectEuler
+//
+//  Created by yury on 03/02/14.
+//  Copyright (c) 2014 yury. All rights reserved.
+//
+
+#ifndef __ProjectEuler__Problem1__
+#define __ProjectEuler__Problem1__
+
+#include <iostream>
+
+#endif /* defined(__ProjectEuler__Problem1__) */
